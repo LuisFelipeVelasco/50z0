5a5111c7f7f634aa17ca10245fdda37a31b1def8
jeronimorojas-cmyk <jeronimo.rojas@correounivalle.edu.co>
2026-07-06 12:10:28 -0500

    updated PlayController and GameView
    fixed error in GameView
    updateBoard Created
    eliminatePlayer updated

--- src/main/java/com/examplez/demo/Controller/PlayController.java
+++ src/main/java/com/examplez/demo/Controller/PlayController.java
@@ -8,43 +8,60 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import javafx.scene.Node;
 
+import java.io.InputStream;
+import java.util.ArrayList;
 import javafx.scene.control.Label;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class PlayController {
     //TASK: card1, card2, card3 are not in the fxml of game view , instead call the vbox that save the four images and then iterate it with getchildren ,
     private Game game;
+
     @FXML
-    private ImageView card1;
+    private VBox vbBot1;
+
     @FXML
-    private ImageView card2;
+    private VBox vbBot2;
+
     @FXML
-    private ImageView card3;
+    private VBox vbBot3;
+    private ArrayList<ImageView> cardsImages;
     @FXML
-    private ImageView card4;
-    private ImageView[] cardsImages;
+    private ImageView imCardInPlay;
     @FXML
-    private ImageView pileCard;
+    private HBox hbPlayerCards;
     @FXML
     private Label labelGame;
     int numberOfPlayers;
     Stage stage;
     public void setStage(Stage stage){this.stage=stage;}
-    public void setNumberOfPlayers(int numberOfPlayers){this.numberOfPlayers=numberOfPlayers;}
+    public void setNumberOfPlayers(int numberOfPlayers) throws InterruptedException {this.numberOfPlayers=numberOfPlayers;
+        updateBoard();
+        startGame();
+    }
     @FXML
 
     //TASK: Generate the cards on the board depending on the number of players
+
     private void initialize() throws InterruptedException {
-        cardsImages = new ImageView[]{
-                card1,
-                card2,
-                card3,
-                card4
-        };
-        startGame();
+
+        cardsImages = new ArrayList<>();
+
+
+        for (Node node : hbPlayerCards.getChildren()) {
+            if (node instanceof ImageView imageView) {
+                cardsImages.add(imageView);
+            }
+        }
+
+
     }
     protected void startGame() throws InterruptedException {
         game = new Game(numberOfPlayers);     // Humano + 3 máquinas
@@ -72,6 +89,7 @@ public class PlayController {
                     //TASK: Delete cards of player in the board
                     else{
                         labelGame.setText("Player "+ t+ "was eliminated");
+
                     }
                 }
             }
@@ -81,17 +99,26 @@ public class PlayController {
     //TASK : image could be null , why?
     protected void showHandCardPlayer(){
         List<Card> hand = game.getHandHumanPlayer();
-        for(int i = 0; i < hand.size(); i++){
+
+        for (int i = 0; i < hand.size(); i++) {
             Card card = hand.get(i);
-            String path =
-            "/com/examplez/demo/images/Cards/"
+
+            String path = "/com/examplez/demo/images/Cards/"
                     + card.getIdCard()
                     + "-"
                     + String.format("%02d", card.getCardValue())
                     + ".png";
-            Image image = new Image(getClass().getResourceAsStream(path));
-            cardsImages[i].setImage(image);
-            addListener(cardsImages[i], card);
+
+            InputStream stream = getClass().getResourceAsStream(path);
+
+            if (stream != null) {
+                Image image = new Image(stream);
+                cardsImages.get(i).setImage(image);
+            } else {
+                System.out.println("Image not found: " + path);
+            }
+
+            addListener(cardsImages.get(i), card);
         }
     }
     protected void addListener(ImageView imageView, Card card){
@@ -114,7 +141,7 @@ public class PlayController {
                 + ".png";
         Image image = new Image(getClass().getResourceAsStream(path));
 
-        pileCard.setImage(image);
+        imCardInPlay.setImage(image);
     }
 
     protected void playCard(Card card){
@@ -131,6 +158,19 @@ public class PlayController {
     //Task: Delete the cards on the view of that player
     protected void eliminatePlayer(int turnPlayer) {
         game.eliminatePlayer(turnPlayer);
+
+        if (turnPlayer==1) {
+            vbBot1.setVisible(false);
+            vbBot1.setManaged(false);
+        }
+        if (turnPlayer==2){
+            vbBot2.setVisible(false);
+            vbBot2.setManaged(false);
+        }
+        if (turnPlayer==3){
+            vbBot3.setVisible(false);
+            vbBot3.setVisible(false);
+        }
     }
 
     private void changeView(int idWinner) throws IOException {
@@ -143,5 +183,16 @@ public class PlayController {
         stage.setScene(scene);
         stage.setResizable(false);
         stage.show();
+    }
+    private void updateBoard() {
+
+        vbBot1.setVisible(numberOfPlayers >= 2);
+        vbBot1.setManaged(numberOfPlayers >= 2);
+
+        vbBot2.setVisible(numberOfPlayers >= 3);
+        vbBot2.setManaged(numberOfPlayers >= 3);
+
+        vbBot3.setVisible(numberOfPlayers >= 4);
+        vbBot3.setManaged(numberOfPlayers >= 4);
     }
 }
